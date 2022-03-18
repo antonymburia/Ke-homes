d718101512c7585111f5497f9b4e0417d49dd78d
antonymburia <antonymburia10@gmail.com>
2022-03-18 10:23:30 +0300

    update files

--- src/app/classes/house.ts
+++ src/app/classes/house.ts
@@ -2,9 +2,11 @@ export class House {
  description: string;
  picture_path:string; 
  price: number;
+ location:string;
 
- constructor(description: string, picture_path: string, price: number) {
+ constructor(description: string, picture_path: string, price: number, location:string) {
   this.description = description;
+  this.location = location;
   this.picture_path = picture_path;
   this.price = price;
  }
